0b5ab29242319f30f9abbb4ddfb4f16e66e1a503
dstritzel <dstritzel@users.noreply.github.com>
2022-12-20 06:08:21 +0000

    Fix downloads attempt 1

--- ubuntu-core/build.sh
+++ ubuntu-core/build.sh
@@ -5,9 +5,9 @@ if [ "${TARGETARCH}" == "arm64" ]; then S6_ARCH="aarch64"; fi
 echo "Downloading files for $S6_ARCH"
 cd /tmp
 
-curl -O -J https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-noarch.tar.xz /tmp
-curl -O -J https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-${S6_ARCH}.tar.xz /tmp
-curl -O -J https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-symlinks-arch.tar.xz /tmp
+curl -O -J https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-noarch.tar.xz
+curl -O -J https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-${S6_ARCH}.tar.xz
+curl -O -J https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-symlinks-arch.tar.xz
 
 echo "*** Building ${TARGETOS} with ${TARGETARCH} with ${S6_ARCH} ***"
 tar -C / -Jxpf /tmp/s6-overlay-noarch.tar.xz
