217fb5521790c7cc1e4d214229f6e52ff0099c23
dstritzel <dstritzel@users.noreply.github.com>
2022-12-20 05:58:21 +0000

    Make a build script

---
+++ ubuntu-core/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ "${TARGETARCH}" == "arm64" ] then; S6_ARCH="aarch64"; fi
+
+echo "Downloading files"
+cd /tmp
+
+curl -O -J https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-noarch.tar.xz /tmp
+curl -O -J https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-${S6_ARCH}.tar.xz /tmp
+curl -O -J https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-symlinks-arch.tar.xz /tmp
+
+echo "*** Building ${TARGETOS} with ${TARGETARCH} with ${S6_ARCH} ***"
+tar -C / -Jxpf /tmp/s6-overlay-noarch.tar.xz
+tar -C / -Jxpf /tmp/s6-overlay-symlinks-arch.tar.xz
+tar -C / -Jxpf /tmp/s6-overlay-${S6_ARCH}.tar.xz
+rm -rf /tmp/*
