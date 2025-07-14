0995c42566a83a23dc31b3da4652dac7cf7cd72f
Derek Stritzel <dstritzel@user.noreply.github.com>
2025-07-14 11:04:38 -0400

    feature tintin Run as specific user
    Fixes #22

--- tintin/tintin-build.sh
+++ tintin/tintin-build.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/sh -x
 # Grab Tintin source
 USER=tintin
+
+apk update && apk upgrade
+apk add curl wget make pcre-dev gnutls-dev zlib-dev build-base
 
 wget -L -O /tmp/tintin.tar.gz \
   "https://github.com/scandum/tintin/releases/download/${TINTIN_VERSION}/tintin-${TINTIN_VERSION}.tar.gz"
@@ -8,8 +11,8 @@ wget -L -O /tmp/tintin.tar.gz \
 cd /tmp
 mkdir /config
 adduser -D $USER
-echo "$USER ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/$USER
-chmod 0440 /etc/sudoers.d/$USER
+#echo "$USER ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/$USER
+#chmod 0440 /etc/sudoers.d/$USER
 chown -R $USER /config
 tar -xf tintin.tar.gz
 
